ab1fee2e336d2bd84166ca7e16dc0fa85a561fec
Jialiang Hu <liang12hour@gmail.com>
2013-10-21 23:26:42 -0400

    added local Id storage and image upload

--- js/picCreation.js
+++ js/picCreation.js
@@ -1,61 +1,70 @@
-var medFiles = [];
-function invoke(num) {
-    try {
-        navigator.device.capture.captureImage(captureSuccess(num), captureError, {limit: 1});
-    } catch (err) {
-        navigator.notification.alert("An error occurred during capture: " + err, null, "Uh oh!");
-    }
-}
+$(document).ready(function(){
+  var medFiles = [];
+  var originalCaller = null;
 
-function captureSuccess(mediaFiles) {
+  $('button').click(function(e) {
     try {
-        var img = mediaFiles[0];
-    	medFiles[num] = img;
-        //navigator.notification.alert("Pushed image to list");
-    	var imageContainer = document.getElementById('imageContainer');
+      //navigator.notification.alert(num);
+      originalCaller = $(this);
+      navigator.device.capture.captureImage(captureSuccess, captureError, {limit: 1});
+    } catch (err) {
+      navigator.notification.alert("An error occurred during capture: " + err, null, "Uh oh!");
+    }
+  });
+
+  function captureSuccess(mediaFiles) {
+    try {
+      var img = mediaFiles[0];
+      medFiles.push = img;
+      uploadFiles();
+      var imageContainer = $('body');
+      for(var medKey in medFiles){
         var imageElement = document.createElement("img");
         imageElement.style.visibility = "visible";
         imageElement.style.display = "block";
         imageElement.style.width = "500px";
-        imageElement.src = img.fullPath;
-        imageContainer.appendChild(imageElement);
-        var upload = document.getElementById('upload');
-        if (upload.innerHTML === "" || upload.innerHTML ===   undefined) {
-           // navigator.notification.alert('in add button block');
-            upload.innerHTML = '<button onclick="uploadFiles()">Upload pictures</button>';
-        }
+        imageElement.src = medFiles[medKey].fullPath;
+        originalCaller.parent().append(imageElement);
+      }
+      var upload = document.getElementById('upload');
     } catch (err) {
-        navigator.notification.alert("Error: " + err);
+      navigator.notification.alert("Error: " + err);
     }
-}
+  }
 
-function captureError(error) {
-	var msg = "An error occurred during capture: " + error.code;
-	navigator.notification.alert(msg, null, "Uh oh!");
-}
+  function captureError(error) {
+    var msg = "An error occurred during capture: " + error;
+    navigator.notification.alert(msg, null, "Uh oh!");
+  }
 
-function uploadFiles() {
+  function uploadFiles() {
     try {
-        var ft = new FileTransfer();
-        for (var index in medFiles){
-            path = medFiles[index].fullPath;
-            name = medFiles[index].name;
-
-            ft.upload(path,
-                "http://cms.Sols.co/api/create_order",
-                function(result) {
-                    navigator.notification.alert('Upload success: ' + result.responseCode);
-                    navigator.notification.alert('bytes sent: ' + result.bytesSent);
-                    navigator.notification.alert('actual size: ' + medFiles[index].size);
-
-                },
-                function(error) {
-                    navigator.notification.alert('Error uploading file ' + path + ': ' + error.code);
-                },
-                { fileName: name }
-            );
-        } 
+      var ft = new FileTransfer();
+      for (var index in medFiles){
+        path = medFiles[index].fullPath;
+        name = medFiles[index].name;
+        navigator.notification.alert(window['localStorage'].id);
+        ft.upload(path,
+          "http://cms.Sols.co/api/create_order?order_id="+window['localStorage'].id,
+          function(result) {
+            navigator.notification.alert('Upload success: ' + result.responseCode);
+            navigator.notification.alert('bytes sent: ' + result.bytesSent);
+            navigator.notification.alert('actual size: ' + medFiles[index].size);
+            navigator.notification.alert('Response message: '+result.response);
+          },
+          function(error) {
+            navigator.notification.alert('Error uploading file ' + path + ': ' + error);
+          },
+          { 
+            fileKey : 'f1',
+            params:{
+              'shoe_size':'1'
+            }
+          }
+        );
+      } 
     } catch(err){
-        navigator.notification.alert("Exception: " + err);
+      navigator.notification.alert("Exception: " + err);
     }
-}
+  }
+});
---
+++ js/picCreation2.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+  var originalCaller = null;
+  var img = null;
+  $('#foot-pics img').click(function(e) {
+    try {
+      //navigator.notification.alert(num);
+      originalCaller = $(this);
+      navigator.device.capture.captureImage(captureSuccess, captureError, {limit: 1});
+    } catch (err) {
+      navigator.notification.alert("An error occurred during capture: " + err, null, "Uh oh!");
+    }
+  });
+
+  function captureSuccess(mediaFiles) {
+    try {
+      img = mediaFiles[0];
+      uploadFiles();
+      var imageElement = originalCaller;
+      //imageElement.css('visibility',"visible");
+     // imageElement.css("display","block");
+      //imageElement.css("width","75px");
+      imageElement.attr("src",img.fullPath);
+      //originalCaller.parent().append(imageElement);
+    } catch (err) {
+      navigator.notification.alert("Error: " + err);
+    }
+  }
+
+  function captureError(error) {
+    var msg = "An error occurred during capture: " + error;
+    navigator.notification.alert(msg, null, "Uh oh!");
+  }
+
+  function uploadFiles() {
+    try {
+      var ft = new FileTransfer();
+      path = img.fullPath;
+      name = img.name;
+      //navigator.notification.alert(window['localStorage'].id);
+      ft.upload(path,
+        "http://cms.Sols.co/api/create_order?order_id="+window['localStorage'].id,
+        function(result) {
+          navigator.notification.alert('Response message: '+result.response);
+          navigator.notification.alert('Upload success: ' + result.responseCode);
+          navigator.notification.alert('bytes sent: ' + result.bytesSent);
+          navigator.notification.alert('actual size: ' + medFiles[index].size);
+        },
+        function(error) {
+          navigator.notification.alert('Error uploading file ' + path + ': ' + error);
+        },
+        { 
+          fileKey : 'f1',
+          params:{
+            'shoe_size':'1'
+          }
+        }
+      );
+    } catch(err){
+      navigator.notification.alert("Exception: " + err);
+    }
+  }
+});
